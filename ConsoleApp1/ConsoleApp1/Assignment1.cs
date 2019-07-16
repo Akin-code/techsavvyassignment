baa3ba8316dde9680699d17c77edc3067c587688
Akin-code <52083244+Akin-code@users.noreply.github.com>
2019-07-16 14:39:32 -0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Assignment1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" List of numbers from 1-20:\n");
+            for (int i = 1; i <= 20; i++)
+          
+                Console.WriteLine("{0}", i);
+            
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //select options
+            Console.WriteLine("Select 1 to print the first 20 numbers");
+            Console.WriteLine("Select 2 to print the  squares of n numbers");
+            Console.WriteLine("Select 3 to print the first 20 numbers");
+            Console.WriteLine("Select 4 to print the first 20 numbers");
+            Console.WriteLine("Select 5 to print the first 20 numbers");
+            Console.WriteLine("Select 6 to print the first 20 numbers");
+            Console.WriteLine("Select 7 to print the first 20 numbers");
+
+            //print first 20 numbers
+
+            Console.WriteLine(" List of numbers from 1-20:\n");
+            for (int i = 1; i <= 20; i++)
+          
+                Console.WriteLine("{0}", i);
+
+
+
+
+            //print the square of n numbers
+            
+           
+            int Sum = 0;
+            Console.WriteLine("Input the number of terms to calculate");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i=1; i<=n; i++)
+            {
+                Console.WriteLine("Input the next number to calculate");
+               int Response= int.Parse( Console.ReadLine());
+
+                Sum = (Sum + Response);
+
+            }
+          int Square = (Sum * Sum);
+            Console.WriteLine("{0}", Square);
+            Console.ReadLine();
+        }
+
+    }
+}
